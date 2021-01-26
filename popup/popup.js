61f2837384d5d3264497c167dd2afd813932b8c9
amanuelanteneh <asa2rc@virginia.edu>
2021-01-26 13:26:26 -0500

    added play/previous

--- popup/popup.js
+++ popup/popup.js
@@ -1,39 +1,64 @@
 var playButton = document.getElementById("myPlayButton");
 var pauseButton = document.getElementById("myPauseButton");
+var nextButton = document.getElementById("myNextButton");
+var prevButton = document.getElementById("myPrevButton");
+var message = "Album art"; //set equal to this for message sent below
 
-function sendPlayMessage(tabs) {
+function sendMessage(tabs) {
     for (let tab of tabs) {
       browser.tabs.sendMessage(
         tab.id,
-        {greeting: "Play"}
-      )
+        {greeting: message}
+      ).then(response => {
+        if (response.response != "0") {
+            document.getElementById("albumArt").src = response.response;
+            document.getElementById("volumeSlider").value = response.volume;
+            }
+        })
     }
   }
 
-function sendPauseMessage(tabs) {
-    for (let tab of tabs) {
-      browser.tabs.sendMessage(
-        tab.id,
-        {greeting: "Pause"}
-      )
-    }
-  }
-
+browser.tabs.query({ /* execute this every time popup is loaded to have album art */
+       currentWindow: true,
+    }).then(sendMessage);
 
 playButton.addEventListener('click', function() {
-    
+    message = "Play";
     browser.tabs.query({
-        currentWindow: true,
-        //active: true
-      }).then(sendPlayMessage);
+        currentWindow: true, //only control from tabs in same browser window
+      }).then(sendMessage);
 
 });
 
 pauseButton.addEventListener('click', function() {
-    
+    message = "Pause";
     browser.tabs.query({
         currentWindow: true,
-        //active: true
-      }).then(sendPauseMessage);
+      }).then(sendMessage);
 
 });
+
+nextButton.addEventListener('click', function() {
+    message = "Next";
+    browser.tabs.query({
+        currentWindow: true,
+      }).then(sendMessage);
+
+});
+
+prevButton.addEventListener('click', function() {
+    message = "Previous";
+    browser.tabs.query({
+        currentWindow: true,
+      }).then(sendMessage);
+
+});
+
+function handleMessage(request, sender, sendResponse) { 
+
+     if (request.greeting == "Song change") {
+        document.getElementById("albumArt").src = request.newURL;
+      }
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
--- yt-music-control.js
+++ yt-music-control.js
@@ -1,17 +1,57 @@
-var player = document.querySelector("video");
+var video = document.querySelector("video");
+var player = document.querySelector('#movie_player');
+var nextButton = document.getElementsByClassName("next-button")[0];
+var previousButton = document.getElementsByClassName("previous-button")[0];
+var albumImageURL = document.querySelector("#song-image").children[0].children[0].src;
+
+if (albumImageURL != null && albumImageURL != undefined) {
+      browser.runtime.sendMessage({
+        greeting: "Song change",
+        newURL: albumImageURL 
+      });
+}
 
 browser.runtime.onMessage.addListener(handleMessage);
 
+/* To check if url changes */
+let lastUrl = location.href; 
+new MutationObserver(() => {
+  const url = location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    onUrlChange();
+  }
+}).observe(document, {subtree: true, childList: true});
+
+function onUrlChange() {
+
+    browser.runtime.sendMessage({
+        greeting: "Song change",
+        newURL: document.querySelector("#song-image").children[0].children[0].src
+    });
+
+}
+
+
 function handleMessage(request, sender, sendResponse) {
-    
-    //console.log("Message from the background script:");
-    //console.log(request.greeting);
+
     if (request.greeting == "Play"){
-        player.play();
+        video.play();
     }
     else if (request.greeting == "Pause") {
-        player.pause();
+        video.pause();
+    }
+    else if (request.greeting == "Next") {
+        nextButton.click();
+    }
+    else if (request.greeting == "Previous") {
+        previousButton.click();
+    }
+    else if (request.greeting == "Album art") { //update album art and volume
+        console.log(document.querySelector(".volume-slider").value );
+        return Promise.resolve({ response: document.querySelector("#song-image").children[0].children[0].src,
+                                volume: document.querySelector("#volume-slider").value });
     }
 
-    //return Promise.resolve({response: "Hi from content script"});    
+    return Promise.resolve({ response: "0" });
 }
